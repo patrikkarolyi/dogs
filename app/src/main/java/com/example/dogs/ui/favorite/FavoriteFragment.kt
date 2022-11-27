d13c044e6d10a9d70aa2bac72e3ac6b5e30e4562
mdt72t <patrikkarolyi@hotmail.com>
2022-11-27 23:23:05 +0100

    Add navigation and view binding

---
+++ app/src/main/java/com/example/dogs/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,22 @@
+package com.example.dogs.ui.favorite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.dogs.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment : Fragment(){
+
+    private lateinit var binding: FragmentFavoriteBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/dogs/ui/list/ListFragment.kt
@@ -0,0 +1,22 @@
+package com.example.dogs.ui.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.dogs.databinding.FragmentListBinding
+
+class ListFragment : Fragment(){
+
+    private lateinit var binding: FragmentListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
